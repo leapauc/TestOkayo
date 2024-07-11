69cd11490fb23f69c2fa84cb5b8c2e329359f42c
lea.pauchot <lea.pauchot@gmail.com>
2024-07-11 12:57:42 +0200

    API_okayo add client and add facture with offers, add example of .json, update README.md

--- API_okayo.js
+++ API_okayo.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2/promise');
 const app = express();
 app.use(express.json());
 
+// Configuration database
 const dbConfig = {
   host: 'localhost',
   user: 'root',
@@ -13,17 +14,60 @@ const dbConfig = {
 
 const db = mysql.createPool(dbConfig);
 
-// Add a new facture
+// Add a new facture with offers
 app.post('/factures', async (req, res) => {
-  console.log('Received request to add new facture');
-  const { dateFacturation, dateEcheance, client_id } = req.body;
-  const query = `INSERT INTO facture (dateFacturation, dateEcheance, client_id) VALUES (?,?,?)`;
+  console.log('Received request to add new facture with offers');
+  const { dateFacturation, dateEcheance, client_id, offers } = req.body;
+
   try {
-    const result = await db.execute(query, [dateFacturation, dateEcheance, client_id]);
-    res.status(201).send({ message: 'Facture added successfully' });
+    // Execute INSERT query for facture creation
+    const [result] = await db.execute(`INSERT INTO facture (dateFacturation, dateEcheance, client_id) VALUES (?,?,?)`, [dateFacturation, dateEcheance, client_id]);
+
+    // Get the insertId from the result to get the last id
+    const factureId = result.insertId;
+
+    if (factureId) {
+      // Add offers to the facture
+      const offerQueries = offers.map((offer) => {
+        const query = `INSERT INTO facture_offre (facture_id, offre_id, quantity, designation_F, prix_F, tauxTVA_F) VALUES (?,?,?,?,?,?)`;
+        console.log(offer.offre_id);
+
+        return db.execute(query, [factureId, offer.offre_id, offer.quantity, offer.designation_F, offer.prix_F, offer.tauxTVA_F]);
+      });
+
+      await Promise.all(offerQueries);
+      res.status(201).send({ message: 'Facture added successfully with offers' });
+    } else {
+      console.error('Facture ID is undefined');
+      res.status(500).send({ message: 'Error adding facture' });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send({ message: 'Error adding facture' });
+  }
+});
+
+// Add a client
+app.post('/clients', async (req, res) => {
+  console.log('Received request to add new client');
+  const { nomEntreprise, rue, codePostal, ville } = req.body;
+
+  try {
+    // Execute the INSERT query
+    const [result] = await db.execute(`INSERT INTO client (nomEntreprise, rue, codePostal, ville) VALUES (?,?,?,?)`, [nomEntreprise, rue, codePostal, ville]);
+
+    // Get the insertId from the result
+    const clientId = result.insertId;
+    console.log(clientId);
+    if (clientId) {
+      res.status(201).send({ message: 'Client added successfully' });
+    } else {
+      console.error('Client ID is undefined');
+      res.status(500).send({ message: 'Error adding client' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Error adding client' });
   }
 });
 
